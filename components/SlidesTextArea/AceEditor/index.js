0431da1eff3b98d791582a027296c1b336acfeff
yushiang <tsengyushiang@gmail.com>
2023-04-23 14:59:17 +0800

    fix: eslint.

--- components/SlidesTextArea/AceEditor/index.js
+++ components/SlidesTextArea/AceEditor/index.js
@@ -2,7 +2,6 @@ import "ace-builds/src-noconflict/ace";
 import "ace-builds/src-noconflict/ext-language_tools";
 import "ace-builds/src-noconflict/mode-java";
 import "ace-builds/src-noconflict/theme-github";
-import React from "react";
 import AceEditor from "react-ace";
 
 const MyAceEditor = ({ value, onChange }) => (
