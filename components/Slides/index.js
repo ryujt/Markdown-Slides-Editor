0fdd72647b6f3e9fb63eb11d2549aae374a2d1d4
yushiang <tsengyushiang@gmail.com>
2023-04-30 23:09:39 +0800

    feature: add print button for exporting pdf.

--- components/Slides/index.js
+++ components/Slides/index.js
@@ -1,5 +1,13 @@
+import { printIframe } from "helpers/file";
+
 const Slides = ({ html }) => {
-  return <iframe style={{ width: "100%", height: "100%" }} srcDoc={html} />;
+  return (
+    <iframe
+      id={printIframe.id}
+      style={{ width: "100%", height: "100%" }}
+      srcDoc={html}
+    />
+  );
 };
 
 export default Slides;
--- containers/ToolBar/index.js
+++ containers/ToolBar/index.js
@@ -1,6 +1,6 @@
 import Icon from "components/Icon";
 import PageTemplateList from "containers/PageTemplateList";
-import { download } from "helpers/file";
+import { download, printIframe } from "helpers/file";
 import { openHtmlWindow } from "helpers/window";
 import { useState } from "react";
 import { useSelector } from "react-redux";
@@ -31,6 +31,7 @@ const ToolBar = () => {
       <IconList>
         <Icon.add onClick={() => setTool(TOOL_TYPE.templates)} />
         {html && <Icon.download onClick={() => download("slide", html)} />}
+        {html && <Icon.print onClick={printIframe.print} />}
         {html && <Icon.newTab onClick={() => openHtmlWindow(html)} />}
       </IconList>
       {Tool && <Tool />}
