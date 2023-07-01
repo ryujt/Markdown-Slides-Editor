f8a2af2a32602c1b69c530cdbed4cd527f5ff700
yushiang <tsengyushiang@gmail.com>
2023-07-01 15:00:15 +0800

    feat: load title and ogTags from server-side in share page.

--- pages/share.js
+++ pages/share.js
@@ -1,5 +1,37 @@
 import Viewer from "apps/viewer";
+import Head from "next/head";
 
-export default function App() {
-  return <Viewer />;
+function Header({ title, ogTitle, ogImage, ogDescription }) {
+  return (
+    <>
+      <Head>
+        <title>{title}</title>
+        <meta property="og:title" content={ogTitle} />
+        <meta property="og:image" content={ogImage} />
+        <meta property="og:description" content={ogDescription} />
+      </Head>
+    </>
+  );
+}
+
+export default function App(props) {
+  return (
+    <>
+      <Header {...props} />
+      <Viewer />
+    </>
+  );
+}
+
+export async function getServerSideProps({ query }) {
+  const { title, ogTitle, ogImage, ogDescription } = query;
+
+  return {
+    props: {
+      title: title || "Default Title",
+      ogTitle: ogTitle || "Defailt Title",
+      ogImage: ogImage || "https://example.com/default-image.jpg",
+      ogDescription: ogDescription || "Default Description",
+    },
+  };
 }
